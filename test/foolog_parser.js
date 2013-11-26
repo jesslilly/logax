2bbf6cea46188c7b00951ed77c21d1308dfee5a0
jmlilly <jmlilly@us.ibm.com>
2013-11-26 14:24:31 -0500

    Start with some test data.

---
+++ test/foolog_parser.js
@@ -0,0 +1,45 @@
+
+var convertToISO = function() {
+	var date = new Date(arguments.$1);
+	return date.toISOString();
+};
+
+var calcArea = function() {
+	return (arguments.$2 - arguments.$1) * (arguments.$4 - arguments.$3);
+};
+
+exports.searchStrings = function() {
+	return [ {
+		searchFor : "^JobID: ([0-9]*)$",
+		sample: "JobID: 12345",
+		outputField : "jobId"
+	}, {
+		searchFor : "^email: (.*)$",
+		sample: "email: abc@abc.com",
+		outputField : "email"
+	}, {
+		searchFor : "^Foo log version: (.*)",
+		sample: "Foo log version: 2.0.1",
+		default: "1.0.0",
+		outputField : "logVersion"
+	}, {
+		searchFor : "^Begin foo log at (.*)$",
+		sample : "Begin foo log at Tue Nov 26 13:50:43 EST 2013",
+		converter: convertToISO,
+		outputField : "startAt"
+	}, {
+		searchFor : "^This thing sometimes happens: ([0-9]*) ([0-9]*) ([0-9]*) ([0-9]*)",
+		sample : "This thing sometimes happens: 1 2400 1 3000",
+		converter: calcArea,
+		outputField : "area"
+	}, {
+		searchFor : "^Elapsed time: ([0-9]*)ms",
+		sample : "Elapsed time: 1000ms",
+		outputField : "elapsedTime"
+	}, {
+		searchFor : "^  End foo log at (.*)$",
+		sample : "  End foo log at Tue Nov 26 13:50:44 EST 2013",
+		converter: convertToISO,
+		outputField : "endAt"
+	} ];
+};
