c5485a81259b86f9fe56acfb475e3a31b62f1069
jmlilly <jmlilly@us.ibm.com>
2013-11-26 23:08:03 -0500

    baby steps.  Parsing works, but test still fails.  Need to implement defaults, converter and output to file.

--- bin/logaxnm.js
+++ bin/logaxnm.js
@@ -1,21 +1,45 @@
+var fs = require('fs');
 
 /**
  * @function
- * @description Parse the input file based on given searchText.
- * write output file.
- * @param {string} path/file of the searchText node module
- * @param {string} path/file of the text file to parse
- * @param {string} path/file of the text file to write.
- * @param {function} callback when it's done (async).
+ * @description Parse the input file based on given searchText. write output
+ *              file.
+ * @param {string}
+ *            path/file of the searchText node module
+ * @param {string}
+ *            path/file of the text file to parse
+ * @param {string}
+ *            path/file of the text file to write.
+ * @param {function}
+ *            callback when it's done (async).
  * @return {void}
  */
 exports.parse = function(searchText, input, output, cb) {
-	
+
+	var retObj = {};
+
 	// TODO: Better way to do this
-	var search = require("../" + searchText);
-	//console.log("Search for " + search.searchStrings);
-	
-	console.log("parse needs to be implemented");
-	
-	cb.call(null);
+	var searches = require("../" + searchText).searchStrings();
+
+	// Open input file
+	require('readline').createInterface({
+		input : fs.createReadStream(input),
+		terminal : false
+	}).on('line', function(line) {
+
+		// Process one line at a time.
+		// (What is more efficient, parsing each line or running many greps?)
+		searches.forEach(function(search) {
+			var matchedText = new RegExp(search.searchFor).exec(line);
+			if (matchedText !== null) {
+				retObj[search.outputField] = matchedText[1];
+			}
+		});
+
+	}).on('close', function() {
+		console.log(retObj);
+
+		cb.call(null);
+	});
+
 };
